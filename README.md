c88c85182ac4bbb899174efc1c84143555697d32
jeffrey82221 <jeffrey82221@gmail.com>
2022-02-23 20:39:12 +0800

    [add] new function and compile

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # python_love_rust
 A playground for building rust binding for python
+
+## Setup 
+
+### Compile the string_sum package
+```
+cd string_sum 
+source .env/bin/activate
+pip install --upgrade pip
+pip install maturin
+maturin develop
+```
+### Use the package
+
+```python
+import string_sum
+string_sum.sum_as_string(5, 20)
+```
+
--- string_sum/src/lib.rs
+++ string_sum/src/lib.rs
@@ -5,13 +5,16 @@ use pyo3::prelude::*;
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
 }
-
+#[pyfunction]
+fn mul_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a*b).to_string())
+}
 /// A Python module implemented in Rust. The name of this function must match
 /// the `lib.name` setting in the `Cargo.toml`, else Python will not be able to
 /// import the module.
 #[pymodule]
 fn string_sum(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-
+    m.add_function(wrap_pyfunction!(mul_as_string, m)?)?;
     Ok(())
 }
