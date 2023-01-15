c67b297d9382a3c387f29a8192d3d39d79abd1cd
jeffrey82221 <jeffrey82221@gmail.com>
2023-01-15 23:07:05 +0800

    feature: add rust testing

--- class_vs_struct/test/test_operation.py
+++ class_vs_struct/test/test_operation.py
@@ -1,7 +1,13 @@
 import python.operation
-
+import operation
 def test_python_class():
     assert python.operation.run_class('add', 1, 2) == 3
     assert python.operation.run_class('mul', 3, 2) == 6
     assert python.operation.run_class('sub', 3, 2) == 1
     assert python.operation.run_class('div', 3, 2) == 1.5
+
+def test_rust_class():
+    assert operation.run_class('add', 1, 2) == 3
+    assert operation.run_class('mul', 3, 2) == 6
+    assert operation.run_class('sub', 3, 2) == 1
+    assert operation.run_class('div', 3, 2) == 1.5
