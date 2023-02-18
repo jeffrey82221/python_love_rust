70cc18f13b680253fb9c0ad68fb7324a48adba27
jeffrey82221 <jeffrey82221@gmail.com>
2023-02-18 22:01:21 +0800

    doc: todo

--- Chap10/rust_objs/src/lib.rs
+++ Chap10/rust_objs/src/lib.rs
@@ -1,3 +1,9 @@
+// Goal: 
+// Make the complex object computation fast. 
+// - The complex object computation should be in the Rust domain (implemented with pure Rust objects). 
+// - A `reduce` method operate on the Rust domain (input: json str list, output: Object str)
+// - The Object str can be transformed to Python object in Python domain. 
+// - When doing `|` operation in Python domain, `or` in Rust domain should be called.  
 // TODO:
 // 1. [ ] build pure rust objects (RustPoint)
 // 2. [ ] Let Point pyclass takes RustPoint as variable. 
@@ -26,7 +32,7 @@ impl Point {
     fn __add__(&self, o: &PyAny) -> PyResult<Point> {
         if let Ok(other_point) = o.extract::<Point>() {
             Ok(Point::new(self.x + other_point.x, self.y + other_point.y))
-        } else if let Ok(other_point2) = o.extract::<Point2>() {
+        } else if let Ok(other_point2) = o.extract::<Point>() {
             Ok(Point::new(self.x + other_point2.x, self.y + other_point2.y))
         } else {
             Err(exceptions::PyTypeError::new_err("Unsupported type"))
