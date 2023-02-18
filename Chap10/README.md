dfd65ae902f053c9c3fefd28a616a74aa20efd4f
jeffrey82221 <jeffrey82221@gmail.com>
2023-02-18 21:36:57 +0800

    feature: add PyClass and Type checking demo

---
+++ Chap10/README.md
@@ -0,0 +1,7 @@
+# Intro 
+
+This is a demo to a cross domain programming of python+rust. 
+
+The `/rust` folder contain a rust-based python package that create rust objects and stored in the domain of Rust.
+
+The `/python` folder contain a pure python package with each python class mapped to each rust object in the `/rust` folder. 
---
+++ Chap10/rust_objs/src/lib.rs
@@ -0,0 +1,61 @@
+// TODO:
+// 1. [ ] build pure rust objects (RustPoint)
+// 2. [ ] Let Point pyclass takes RustPoint as variable. 
+// 3. [ ] Let RustPoint be able to be converted to a str ("Point(x, y)")
+// 4. [ ] Implement methods on RustPoint and call them from the Python Point method. 
+use pyo3::prelude::*;
+use pyo3::exceptions;
+#[derive(Clone)]
+#[pyclass]
+struct Point {
+    #[pyo3(get)]
+    x: i32,
+    #[pyo3(get)]
+    y: i32,
+}
+
+#[pymethods]
+impl Point {
+    #[new]
+    fn new(x: i32, y: i32) -> Self {
+        Point { x, y }
+    }
+    fn distance_from_origin(&self) -> f64 {
+        ((self.x.pow(2) + self.y.pow(2)) as f64).sqrt()
+    }
+    fn __add__(&self, o: &PyAny) -> PyResult<Point> {
+        if let Ok(other_point) = o.extract::<Point>() {
+            Ok(Point::new(self.x + other_point.x, self.y + other_point.y))
+        } else if let Ok(other_point2) = o.extract::<Point2>() {
+            Ok(Point::new(self.x + other_point2.x, self.y + other_point2.y))
+        } else {
+            Err(exceptions::PyTypeError::new_err("Unsupported type"))
+        }
+    }
+}
+
+#[derive(Clone)]
+#[pyclass]
+struct Point2 {
+    #[pyo3(get)]
+    x: i32,
+    #[pyo3(get)]
+    y: i32,
+}
+
+#[pymethods]
+impl Point2 {
+    #[new]
+    fn new(x: i32, y: i32) -> Self {
+        Point2 { x, y }
+    }
+}
+
+
+
+#[pymodule]
+fn rust_objs( _py: Python, m: &PyModule ) -> PyResult<()> {
+    m.add_class::<Point>()?;
+    m.add_class::<Point2>()?;
+    return Ok( () );
+}
