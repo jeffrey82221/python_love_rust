bc8d5e846ff8a21966621fe0fe7f86867a950341
jeffrey82221 <jeffrey82221@gmail.com>
2023-02-21 10:53:19 +0800

    feature: add Atomic Obj for both domain

--- Chap10/rust_objs/src/lib.rs
+++ Chap10/rust_objs/src/lib.rs
@@ -5,63 +5,52 @@
 // - The Object str can be transformed to Python object in Python domain. 
 // - When doing `|` operation in Python domain, `or` in Rust domain should be called.  
 // TODO:
-// 1. [ ] build pure rust objects (RustPoint)
+// 1. [ ] build pure rust objects (RustJsonSchema)
 // 2. [ ] Let Point pyclass takes RustPoint as variable. 
 // 3. [ ] Let RustPoint be able to be converted to a str ("Point(x, y)")
 // 4. [ ] Implement methods on RustPoint and call them from the Python Point method. 
 use pyo3::prelude::*;
-use pyo3::exceptions;
+
 #[derive(Clone)]
-#[pyclass]
-struct Point {
-    #[pyo3(get)]
-    x: i32,
-    #[pyo3(get)]
-    y: i32,
+struct RustAtomic {
+    content: i32   
+}
+impl RustAtomic {
+    fn new(content: i32) -> RustAtomic {
+        RustAtomic { content }
+    }
+    
+}
+impl IntoPy<PyObject> for RustAtomic {
+    fn into_py(self, py: Python) -> PyObject {
+        py.None()
+    }
 }
 
-#[pymethods]
-impl Point {
-    #[new]
-    fn new(x: i32, y: i32) -> Self {
-        Point { x, y }
-    }
-    fn distance_from_origin(&self) -> f64 {
-        ((self.x.pow(2) + self.y.pow(2)) as f64).sqrt()
-    }
-    fn __add__(&self, o: &PyAny) -> PyResult<Point> {
-        if let Ok(other_point) = o.extract::<Point>() {
-            Ok(Point::new(self.x + other_point.x, self.y + other_point.y))
-        } else if let Ok(other_point2) = o.extract::<Point>() {
-            Ok(Point::new(self.x + other_point2.x, self.y + other_point2.y))
-        } else {
-            Err(exceptions::PyTypeError::new_err("Unsupported type"))
-        }
-    }
-}
 
 #[derive(Clone)]
 #[pyclass]
-struct Point2 {
+struct Atomic {
     #[pyo3(get)]
-    x: i32,
-    #[pyo3(get)]
-    y: i32,
+    rust_obj: RustAtomic,
 }
 
 #[pymethods]
-impl Point2 {
+impl Atomic {
     #[new]
-    fn new(x: i32, y: i32) -> Self {
-        Point2 { x, y }
+    fn new(content: i32) -> Self {
+        let x = RustAtomic {content};
+        Atomic { rust_obj: x }
+    }
+
+    fn __repr__(&self) -> String {
+        format!("Atomic({})", self.rust_obj.content)
     }
 }
-
 
 
 #[pymodule]
 fn rust_objs( _py: Python, m: &PyModule ) -> PyResult<()> {
-    m.add_class::<Point>()?;
-    m.add_class::<Point2>()?;
+    m.add_class::<Atomic>()?;
     return Ok( () );
 }
