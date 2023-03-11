40a9d4030c560a68ee63bfa09e91fbbc25ec3c66
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-11 10:39:52 +0800

    fix: flake8

--- Chap10/rust_objs/tests/test_infer.py
+++ Chap10/rust_objs/tests/test_infer.py
@@ -2,9 +2,11 @@ import pytest
 from rust_objs import InferenceEngine
 from rust_objs import *
 
+
 @pytest.fixture
 def engine():
     return InferenceEngine()
+
 
 @pytest.fixture
 def test_data():
@@ -12,16 +14,24 @@ def test_data():
         (["1"], "Atomic(Int())"),
         (["1.2"], "Atomic(Float())"),
         (["[1]"], "Array(Atomic(Int()))"),
-        (['{"a": 1, "b": 2.0}'], 'Record({"a": Atomic(Int()), "b": Atomic(Float())})'),
+        (['{"a": 1, "b": 2.0}'],
+         'Record({"a": Atomic(Int()), "b": Atomic(Float())})'),
         (["1", "null"], 'Optional(Atomic(Int()))'),
-        (["1", "1.2", "null"], 'Union({Atomic(Float()), Atomic(Int()), Atomic(Non())})'),
-        (['{"a": 1, "b": 2.0}', '{"a": 1, "b": 5.0}'], 'Record({"a": Atomic(Int()), "b": Atomic(Float())})'),
+        (["1", "1.2", "null"],
+         'Union({Atomic(Float()), Atomic(Int()), Atomic(Non())})'),
+        (['{"a": 1, "b": 2.0}', '{"a": 1, "b": 5.0}'],
+         'Record({"a": Atomic(Int()), "b": Atomic(Float())})'),
         ([], 'Unknown()'),
-        (['{"1": {"a": 5, "b": 6.0}, "2": {"a": 34, "b": 3.0 } }'], 'UniformRecord(FieldSet({"1", "2"}), Record({"a": Atomic(Int()), "b": Atomic(Float())}))'),
-        (['{"1": [{"a": 5, "b": 6.0}], "2": [{"a": 34, "b": 3.0 }] }'], 'UniformRecord(FieldSet({"1", "2"}), Array(Record({"a": Atomic(Int()), "b": Atomic(Float())})))'),
-        (['{"1": [{"a": 5, "b": 6.0}], "2": [] }'], 'UniformRecord(FieldSet({"1", "2"}), Array(Record({"a": Atomic(Int()), "b": Atomic(Float())})))'),
-        (['{"1": [{"a": 5, "b": 6.0}], "2": [{"a": 34, "b": null }] }'], 'UniformRecord(FieldSet({"1", "2"}), Array(Record({"a": Atomic(Int()), "b": Optional(Atomic(Float()))})))'),
+        (['{"1": {"a": 5, "b": 6.0}, "2": {"a": 34, "b": 3.0 } }'],
+         'UniformRecord(FieldSet({"1", "2"}), Record({"a": Atomic(Int()), "b": Atomic(Float())}))'),
+        (['{"1": [{"a": 5, "b": 6.0}], "2": [{"a": 34, "b": 3.0 }] }'],
+         'UniformRecord(FieldSet({"1", "2"}), Array(Record({"a": Atomic(Int()), "b": Atomic(Float())})))'),
+        (['{"1": [{"a": 5, "b": 6.0}], "2": [] }'],
+         'UniformRecord(FieldSet({"1", "2"}), Array(Record({"a": Atomic(Int()), "b": Atomic(Float())})))'),
+        (['{"1": [{"a": 5, "b": 6.0}], "2": [{"a": 34, "b": null }] }'],
+         'UniformRecord(FieldSet({"1", "2"}), Array(Record({"a": Atomic(Int()), "b": Optional(Atomic(Float()))})))'),
     ]
+
 
 def test_schema_class_ok(test_data):
     for _, schema_str in test_data:
